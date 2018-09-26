da76549d1fb5c71d7d8d0728fa3d16a7ff07ab54
josieperalta <josieperalta@ool-ad0205a5.dyn.optonline.net>
2018-09-26 09:37:18 -0400

    started game code

---
+++ app.js
@@ -0,0 +1,8 @@
+let player1 = document.querySelector('.user')
+
+player1.addEventListener('click', function() {
+  let player1 = false;
+  if(player1 != true) {
+    player1.style.bottom = 100px;
+  }
+})
