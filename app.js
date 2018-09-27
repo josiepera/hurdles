e4015080988ad63960ccd46b7c7cd666c0d94720
josieperalta <josieperalta@ool-ad0205a5.dyn.optonline.net>
2018-09-27 10:52:44 -0400

    multiple hurdles added

--- app.js
+++ app.js
@@ -19,52 +19,42 @@ function down () {
 
 //use the DOM to create more hurdles, a finite amount
 
+// 
+// function hurdles(){
+// let newDiv = document.createElement('div');
+// let newHurdle = newDiv.classList.add('hurdle');
+//
+// document.querySelector('.game-box').appendChild(newDiv);
+//
+// return newDiv;
+//
+// }
+//
+//
+// setTimeout(hurdles, 2000);
+//
+// for(i = 0; i <= 5; i++) {
+//   hurdles();
+//
+// }
 
-function hurdles(){
-let newDiv = document.createElement('div');
-let newHurdle = newDiv.classList.add('hurdle');
-
-document.querySelector('.game-box').appendChild(newDiv);
-
-return newDiv;
-
-}
-
-
-setTimeout(hurdles, 2000);
-
-for(i = 0; i < 5; i++) {
-  hurdles();
-
-}
-
-function collision() {
-
-    // let playerW = player.offsetWidth;
-    // let playerH = player.offsetHeight;
-    // let playerX = player.offsetLeft;
-    // let playerY = player.offsetTop;
-    // let hurdleW = hurdle.offsetWidth;
-    // let hurdleH = hurdle.offsetHeight;
-    // let hurdleX = hurdle.offsetLeft;
-    // let hurdleY = hurdle.offsetTop;
-    //
-    // if ((player1.left + player1.width) > hurdle.left &&
-    //     player1.left < (hurdle.left + hurdle.width) &&
-    //     (player1.top + player1.height) > player1.height &&
-    //     player1.top < (hurdle.top + Hurdle.height)) {
-    //       console.log('collision');
-    //     };
-    if (player1.right = hurdle.left) {
-        console.log('collision');
-      } else if (player1.bottom = hurdle.top) {
-        console.log('collision')
-      }
-}
-collision()
-
-
-// if (player.left < hurdle.left + hurdle.width  && player.left + player.width  > hurdle.left &&
-// 		player.top < hurdle.top + hurdle.height && player.top + player.height > hurdle.top) {
-//     console.log('collision');
-//   }
+// function collision() {
+//     // if ((player1.left + player1.width) > hurdle.left &&
+//     //     player1.left < (hurdle.left + hurdle.width) &&
+//     //     (player1.top + player1.height) > player1.height &&
+//     //     player1.top < (hurdle.top + Hurdle.height)) {
+//     //       console.log('collision');
+//     //     };
+//     if (player1.right = hurdle.left) {
+//         console.log('collision');
+//       } else if (player1.bottom = hurdle.top) {
+//         console.log('collision')
+//       }
+// }
+// collision()
+//
+//
+// // if (player.left < hurdle.left + hurdle.width  && player.left + player.width  > hurdle.left &&
+// // 		player.top < hurdle.top + hurdle.height && player.top + player.height > hurdle.top) {
+// //     console.log('collision');
+// //   }
