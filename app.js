44fb9ec0150eb52a5967595a5dfc955a21f27e59
josieperalta <josieperalta@ool-ad0205a5.dyn.optonline.net>
2018-09-28 14:25:43 -0400

    horizontal collision

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 
-
 let hurdle = document.querySelector('.hurdle')
 let player1 = document.querySelector('.user')
+let hurdleOne = document.querySelector('#one')
 
+// let hurdleOneRight = document.querySelector('#one')
+// console.log(window.getComputedStyle(hurdleOneRight, null).getPropertyValue("right"));
 
-// player1.addEventListener('click', function() {
-//     player1.style.bottom = '250px';
+// let hurdleOneTop = document.querySelector('#one')
+// console.log(window.getComputedStyle(hurdleOneTop, null).getPropertyValue("height"));
 //
-// setTimeout(down, 1000);
+// let player1Bottom = document.querySelector('#one')
+// console.log(window.getComputedStyle(Player1Bottom, null).getPropertyValue("bottom"));
 //
-// })
+// let player1Right = document.querySelector('#one')
+// console.log(window.getComputedStyle(playerOneRight, null).getPropertyValue("right"));
+
 
 document.addEventListener('keydown', function jump(event){
   if(event.keyCode == 38) {
     player1.style.bottom = '250px';
-    //if (player1Right == hurdleLeft ) {
-    // alert('crash')
 setTimeout(down, 800);
-  }
-
-
+}
 })
-
 function down () {
-  player1.style.bottom = '5px'
+  player1.style.bottom = '0px'
 }
 
 
@@ -34,18 +34,46 @@ function myFunction() {
 
 
 
+function isCollapsed(player1, hurdle){
 
-// function collision() {
-//     // if ((player1.left + player1.width) > hurdle.left &&
-//     //     player1.left < (hurdle.left + hurdle.width) &&
-//     //     (player1.top + player1.height) > player1.height &&
-//     //     player1.top < (hurdle.top + Hurdle.height)) {
-//     //       console.log('collision');
-//     //     };
-//     if (player1.right = hurdle.left) {
-//         console.log('collision');
-//       } else if (player1.bottom = hurdle.top) {
-//         console.log('collision')
-//       }
-// }
-// collision()
+  let playerLeft = window.getComputedStyle(player1, null).getPropertyValue("left");
+  console.log('person' + playerLeft)
+
+  let hurdleLeft = window.getComputedStyle(hurdle, null).getPropertyValue("left");
+console.log('hurdle' + hurdleLeft)
+
+
+
+playerLeft = parseInt(playerLeft.split('px')[0])
+hurdleLeft = parseInt(hurdleLeft.split('px')[0])
+
+if (hurdleLeft <= playerLeft + 60){
+  alert('you lose')
+}
+ // if (object_1.left < object_2.left + object_2.width  && object_1.left + object_1.width  > object_2.left &&
+ //   object_1.top < object_2.top + object_2.height && object_1.top + object_1.height > object_2.top) {
+ //   alert("collide");
+ // }
+
+ // else{
+ //   rect.classList.remove("collide");
+ // }
+}
+setInterval(function(){
+  isCollapsed(player1, hurdle)
+}, 300)
+
+
+// let sI = setInterval( () => {
+//       console.log('inside setInterval')
+//
+//       right = parseInt(right.split('px')[0])
+//       if(right > 783) {
+//       console.log('this is right: ', right)
+//        document.removeEventListener('keydown', jump)
+//         document.player1.style.animationPlayState = 'paused';
+//        clearInterval(sI)
+//      }
+//     },50)
+
+// alert('crash')
